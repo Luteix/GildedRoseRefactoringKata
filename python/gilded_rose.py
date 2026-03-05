d94d4fdfa9c66b38b46bebfbc357ee3f4622e887
Luteix <luteix2002@gmail.com>
2026-03-05 11:44:06 +0100

    mise au propre code + rajout de conjured

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,20 +5,19 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+def update_quality(self):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name == "Aged Brie":
+            elif item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
                 item.sell_in -= 1
                 if item.sell_in < 0 and item.quality < 50:
                     item.quality += 1
-                continue
 
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality += 1
                     if item.sell_in < 11 and item.quality < 50:
@@ -28,13 +27,22 @@ class GildedRose(object):
                 item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality = 0
-                continue
 
-            if item.quality > 0:
-                item.quality -= 1
-            item.sell_in -= 1
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality -= 1
+            elif item.name == "Conjured Mana Cake":
+                if item.quality > 0:
+                    item.quality -= 2
+                item.sell_in -= 1
+                if item.sell_in < 0 and item.quality > 0:
+                    item.quality -= 2
+                if item.quality < 0:
+                    item.quality = 0
+
+            else:
+                if item.quality > 0:
+                    item.quality -= 1
+                item.sell_in -= 1
+                if item.sell_in < 0 and item.quality > 0:
+                    item.quality -= 1
 
 
 class Item:
